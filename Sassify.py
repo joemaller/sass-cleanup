508ed6015aeae491a111bd92fdcd28fc798e2395
Joe Maller <joe@joemaller.com>
2013-06-10 02:58:13 -0400

    Added is_enabled to commands, compile_in_place and fixed the problem where first-lines weren't expanding to catch their rules

--- Sassify.py
+++ Sassify.py
@@ -30,6 +30,7 @@ print("loaded Sassify")
 
 APP_NAME = "Sass Beautifier"
 SASS_BIN_DIR = ''
+SETTINGS = {}
 
 
 def plugin_loaded():
@@ -37,17 +38,14 @@ def plugin_loaded():
     print('*************************PLUGIN_LOADED*******************')
     SETTINGS = sublime.load_settings('Sassify.sublime-settings')
 
-    print(SETTINGS.get('sass_bin_dir'))
-    print('settings has', SETTINGS.has('sass_bin_dir'))
+    # print("__file__\n", __file__)
+    # print("os.path.dirname(__file__)\n",
+    #       os.path.dirname(__file__))
+    # print("os.path.dirname(os.path.abspath(__file__))\n",
+    #       os.path.dirname(os.path.abspath(__file__)))
 
-    print("__file__\n", __file__)
-    print("os.path.dirname(__file__)\n",
-          os.path.dirname(__file__))
-    print("os.path.dirname(os.path.abspath(__file__))\n",
-          os.path.dirname(os.path.abspath(__file__)))
-
-    print("sublime.installed_packages_path())\n",
-          sublime.installed_packages_path())
+    # print("sublime.installed_packages_path())\n",
+    #       sublime.installed_packages_path())
 
     # TODO: Clean this mess up
     # TODO: Check for existing sass version to see if we need an update
@@ -59,7 +57,28 @@ def plugin_loaded():
     print('SASS_BIN_DIR', SASS_BIN_DIR)
     sass.make_it_work()
 
-# TODO: Add the sass_installer here.
+def is_css(view):
+    pt = view.sel()[0].begin()
+    return bool(view.score_selector(pt, 'source.css'))
+
+
+def is_scss(view):
+    pt = view.sel()[0].begin()
+    return bool(view.score_selector(pt, 'source.scss'))
+
+
+def is_sass(view):
+    pt = view.sel()[0].begin()
+    return bool(view.score_selector(pt, 'source.sass'))
+
+
+def is_enabled(view):
+    """
+    Checks that pt in view is either SCSS or CSS but not Sass
+    Wraps up is_css, is_scss and is_sass
+    """
+    return (is_css(view) or is_scss(view)) and not is_sass(view)
+
 
 class SassBaseCommand(sublime_plugin.TextCommand):
     def update(self, edit, result):
@@ -74,12 +93,15 @@ class SassBaseCommand(sublime_plugin.TextCommand):
 
     def process(self, **kwargs):
         app = kwargs.get('app', sass_convert)
+        preserve_src = kwargs.get('preserve_src')
         defaults = {'sass_bin_dir': SASS_BIN_DIR}
         defaults.update(kwargs)
         dest = error = ''
         try:
             src = self.view.substr(self.view.sel()[0])
             dest, error = app(src, **defaults)
+            if preserve_src:
+                dest = '\n'.join(['/*', src, '*/', dest])
         except Exception as e:
             sublime.status_message("*!* {}: {}".format(APP_NAME, e))
             error = str(e)
@@ -98,12 +120,30 @@ class SassConvertCommand(SassBaseCommand):
 
 
 class SassCompileCompactCommand(SassBaseCommand):
+    def is_enabled(self):
+        return is_css(self.view)
+
     def run(self, edit):
-        print('hallooo!')
         self.format_and_replace(edit, app=sass_compile, format="compact")
 
 
+class SassCompileInPlaceCommand(SassBaseCommand):
+    def is_enabled(self):
+        return is_enabled(self.view)
+
+    def run(self, edit):
+        preserve_src = SETTINGS.get('preserve_scss_in_comment', False)
+        self.format_and_replace(
+            edit,
+            app=sass_compile,
+            format="compact",
+            preserve_src=preserve_src)
+
+
 class ExpandSelectionToCssRuleCommand(sublime_plugin.TextCommand):
+    def is_enabled(self):
+        return is_enabled(self.view)
+
     def run(self, edit):
         self.expand_selection_to_rule()
 
@@ -115,10 +155,17 @@ class ExpandSelectionToCssRuleCommand(sublime_plugin.TextCommand):
         # TODO: Make multiple selections work right...
         #           for region in self.view.sel():
 
-        # TODO: pre-check line for last-character or first-char next line is "{"
-        # if that's true, select hte line before doing anything else.
         old_sel = self.view.sel()[0]
         rule = self.view.substr(old_sel)
+
+        # Pre-check line for last-character or first-char next line is "{"
+        # if that's true, then reset old_sel and rule and select the line
+        # before doing anything else.
+        if re.search(r"\{\s*", self.view.substr(self.view.line(old_sel.end()))):
+            self.view.sel().clear()
+            self.view.sel().add(self.view.line(old_sel.end()).end())
+            old_sel = self.view.sel()[0]
+            rule = self.view.substr(old_sel)
 
         # if we've got brackets, reduce the selection down to just the brackets
         # so Expand Selection to Brackets will grab the parent.
@@ -127,12 +174,6 @@ class ExpandSelectionToCssRuleCommand(sublime_plugin.TextCommand):
             self.view.sel().add(sublime.Region(
                 old_sel.begin() + rule.index('{'),
                 old_sel.begin() + rule.rindex('}') + 1))
-
-        # special case for opening line of a css/scss rule
-        if rule.count('{') > rule.count('}'):
-            self.view.sel().clear()
-            p = old_sel.begin() + rule.rindex('{')
-            self.view.sel().add(sublime.Region(p, p))
 
         self.view.run_command("expand_selection", {"to": "brackets"})
         self.view.sel().add(self.view.line(self.view.sel()[0]))
